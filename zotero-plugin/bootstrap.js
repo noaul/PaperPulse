bf264502815c966286a1c19cc253a2cc1635289f
noaul <noaul@users.noreply.github.com>
2026-05-12 11:57:45 +0800

    fix: make zotero plugin settings discoverable

--- zotero-plugin/bootstrap.js
+++ zotero-plugin/bootstrap.js
@@ -1,4 +1,5 @@
 var PaperPulseZoteroAnalyzer;
+var chromeHandle;
 
 function log(message) {
   Zotero.debug(`[PaperPulse] ${message}`);
@@ -6,17 +7,47 @@ function log(message) {
 
 async function install() {}
 
-async function startup({ rootURI }) {
-  Services.scriptloader.loadSubScript(`${rootURI}content/paperpulse.js`);
-  PaperPulseZoteroAnalyzer = new PaperPulse.ZoteroAnalyzer(rootURI);
+async function startup({ id, resourceURI, rootURI }) {
+  await Zotero.initializationPromise;
+  if (!rootURI) {
+    rootURI = resourceURI.spec;
+  }
+
+  const aomStartup = Components.classes[
+    "@mozilla.org/addons/addon-manager-startup;1"
+  ].getService(Components.interfaces.amIAddonManagerStartup);
+  const manifestURI = Services.io.newURI(`${rootURI}manifest.json`);
+  chromeHandle = aomStartup.registerChrome(manifestURI, [
+    ["content", "paperpulse", `${rootURI}content/`],
+  ]);
+
+  const ctx = { rootURI };
+  ctx._globalThis = ctx;
+  Services.scriptloader.loadSubScript(`${rootURI}content/paperpulse.js`, ctx);
+  PaperPulseZoteroAnalyzer = new ctx.PaperPulse.ZoteroAnalyzer(rootURI);
   await PaperPulseZoteroAnalyzer.startup();
   log("started");
 }
 
-async function shutdown() {
+async function onMainWindowLoad({ window }) {
+  PaperPulseZoteroAnalyzer?.onMainWindowLoad(window);
+}
+
+async function onMainWindowUnload({ window }) {
+  PaperPulseZoteroAnalyzer?.onMainWindowUnload(window);
+}
+
+async function shutdown(data, reason) {
+  if (reason === APP_SHUTDOWN) {
+    return;
+  }
   if (PaperPulseZoteroAnalyzer) {
     await PaperPulseZoteroAnalyzer.shutdown();
     PaperPulseZoteroAnalyzer = null;
+  }
+  if (chromeHandle) {
+    chromeHandle.destruct();
+    chromeHandle = null;
   }
   log("stopped");
 }
